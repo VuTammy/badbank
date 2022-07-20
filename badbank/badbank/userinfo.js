aab9aba7b4fe45a053edea2db32aa99864999c5c
Tammy <97935337+VuTammy@users.noreply.github.com>
2022-07-19 21:36:00 -0500

    Delete badbank/badbank directory
    undo first commit

--- badbank/badbank/userinfo.js
+++
@@ -1,9 +0,0 @@
-var createName = [];
-var createEmail = [];
-var createPassword = [];
-
-function UpdateData(x, y, z){
-    createName.push(x);
-    createEmail.push(y);
-    createPassword.push(z);
-}
